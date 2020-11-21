a27ac03a99eb02dfacdff89ab874cf67abfd95e8
MOJEED KUSIMO <mkusimo90@gmail.com>
2020-11-21 21:48:01 +0100

    Completed the node.js exercise

--- node/npm/main.js
+++ node/npm/main.js
@@ -1,10 +1,37 @@
-const request = require("request");
+const fs = require("fs");
+const curl = new (require("curl-request"))();
+const prompt = require("prompt");
 
-request("https://jsonmock.hackerrank.com/api/football_matches?year=2011", (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-        console.log(JSON.parse(body));
-    }
-    else {
-        console.log(body)
-    }
-});
+prompt.start();
+
+prompt.get(["searchTerm"], (error, result) => {
+    if (error) throw error;
+    findJoke(result.searchTerm);
+})
+// let searchParameter = process.argv[2];
+
+let findJoke = (searchTerm) => {
+    curl.setHeaders([
+        "Accept: application/json"
+    ])
+    .get(`https://icanhazdadjoke.com/search?term=${searchTerm}`)
+    .then(({statusCode, body, headers}) => {
+        let jokesArray = body.results;
+    
+        if (jokesArray.length === 0) {
+            console.log("I am sorry no joke was found for the word you entered, please try another word.");
+        } else{
+    
+            let randomNum = Math.floor(Math.random()*jokesArray.length);
+            let randomJoke = jokesArray[randomNum].joke;
+            
+            fs.appendFile("jokes.txt", `\n${randomJoke}`, (error) => {
+                if (error) throw error;
+                console.log("joke.txt has been updated!");
+            });
+        }
+    })
+    .catch((e) => {
+        console.log(e);
+    });    
+}
